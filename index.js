29adee103316cac75df1e2f6dde13327d636696c
Jack W. Tussing <58670450+tussingj89@users.noreply.github.com>
2021-02-07 21:13:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var table = require("console.table");
+var chalk = require("chalk");
 
+let roleArr = [];
+let deptArr = [];
+let employeeArr = [];
+let managerArr = [];
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -20,18 +25,82 @@ var connection = mysql.createConnection({
   // connect to the mysql server and sql database
   connection.connect(function(err) {
     if (err) throw err;
-    runSearch;
+    console.log("Connected as Id" + connection.threadId)
+    startApp();
+    setList(); 
+    runSearch();
 });
+function setList() {
+connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push({name: res[i].title, id: res[i].id});
+    }
+    // console.table(roleArr)
+})
+connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      employeeArr.push({name: res[i].last_name, id: res[i].id});
+    }
+    // console.table(employeeArr)
+})
+connection.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      deptArr.push({name: res[i].name, id: res[i].id});
+    }
+    // console.table(deptArr)
+})
+connection.query("SELECT * FROM employee WHERE role_id= 2", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managerArr.push({name: res[i].last_name, id: res[i].id});
+    }
+    // console.table(managerArr)
+})
+}
+function startApp() {
+	console.log(
+		chalk.blueBright(`
+---------------------------------------------------------------------------------------                                                
+     ________                          __                                              
+    /        |                        /  |                                             
+    $$$$$$$$/  _____  ____    ______  $$ |  ______   __    __   ______    ______       
+    $$ |__    /     \/    \  /      \ $$ | /      \ /  |  /  | /      \  /      \      
+    $$    |   $$$$$$ $$$$  |/$$$$$$  |$$ |/$$$$$$  |$$ |  $$ |/$$$$$$  |/$$$$$$  |     
+    $$$$$/    $$ | $$ | $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$    $$ |$$    $$ |     
+    $$ |_____ $$ | $$ | $$ |$$ |__$$ |$$ |$$ \__$$ |$$ \__$$ |$$$$$$$$/ $$$$$$$$/      
+    $$       |$$ | $$ | $$ |$$    $$/ $$ |$$    $$/ $$    $$ |$$       |$$       |     
+    $$$$$$$$/ $$/  $$/  $$/ $$$$$$$/  $$/  $$$$$$/   $$$$$$$ | $$$$$$$/  $$$$$$$/      
+                            $$ |                    /  \__$$ |                         
+                            $$ |                    $$    $$/                          
+                            $$/                      $$$$$$/                           
+     __       __                                                                       
+    /  \     /  |                                                                      
+    $$  \   /$$ |  ______   _______    ______    ______    ______    ______            
+    $$$  \ /$$$ | /      \ /       \  /      \  /      \  /      \  /      \           
+    $$$$  /$$$$ | $$$$$$  |$$$$$$$  | $$$$$$  |/$$$$$$  |/$$$$$$  |/$$$$$$  |          
+    $$ $$ $$/$$ | /    $$ |$$ |  $$ | /    $$ |$$ |  $$ |$$    $$ |$$ |  $$/           
+    $$ |$$$/ $$ |/$$$$$$$ |$$ |  $$ |/$$$$$$$ |$$ \__$$ |$$$$$$$$/ $$ |                
+    $$ | $/  $$ |$$    $$ |$$ |  $$ |$$    $$ |$$    $$ |$$       |$$ |                
+    $$/      $$/  $$$$$$$/ $$/   $$/  $$$$$$$/  $$$$$$$ | $$$$$$$/ $$/                 
+                                               /  \__$$ |                              
+                                               $$    $$/                               
+                                                $$$$$$/                                                                    
+-----------------------------------------------------------------------------------------
+    `)
+	);
+    };
 // start of the inquirer prompts
 function runSearch() {
     inquirer.prompt({
         type: "list",
         message: "What would you like to do?",
-        Choices: [
+        choices: [
          "add to company",
          "view company", 
-         "update employee records",
-         "edit company records", // do i need if i can view update and delete some where else
+         "update records",
          "delete from the company",
          "exit"],
          name: "action"
@@ -43,7 +112,7 @@ function runSearch() {
             
             addToCompany();
 
-		} else if (input.action === 'view company ') {
+		} else if (input.action === 'view company') {
 			
 			viewCompany();
 
@@ -51,10 +120,6 @@ function runSearch() {
 			
 			updateCompany();
 
-		} else if(input.action === "edit company records") {
-			
-            editCompany();
-        
         } else if (input.action === "delete from the company") {
             
             deleteFromCompany();
@@ -83,7 +148,7 @@ function addToCompany() {
     if (input.add_type === 'department') {
         adddepartment();
 
-    } else if (input.add_type === 'roles') {
+    } else if (input.add_type === 'role') {
         addroles();
 
     } else if (input.add_type === 'employee') {
@@ -104,26 +169,45 @@ function addEmployee() {
         name: "last_name"
     },
     {
-        type: "input",
-        message: "what is the employees role id number",
-        name: "roleID"
+        type: "list",
+        message: "what is there role",
+        choices: roleArr,
+        name: "newrole"
     },
     {
-        type: "input",
-        message: "what is the managers id number",
-        name: "managersID"
+        type: "list",
+        message: "who is the manager",
+        choices: managerArr,
+        name: "manager"
+        
 
     }]).then(function(answer){
-        connection.query("INSERT INTO employee (first_name, last_name, role_id, managers_id) VALUES (?, ?, ?, ?)", [answer.first_name, answer.last_name, answer.roleID, answer.managersID] , function(err, res) {
+
+     let roleID1;
+        for (i=0; i < roleArr.length; i++){
+            if (answer.newrole == roleArr[i].name){
+                roleID1 = roleArr[i].id;
+            }
+        }
+
+        let manager;
+        for (i=0; i < managerArr.length; i++){
+            if (answer.manager == managerArr[i].name){
+                manager = managerArr[i].id;
+            }
+            // console.log(manager)
+        }
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.first_name, answer.last_name, roleID1, manager] , function(err, res) {
         if (err) throw err;
-        console.table(res + "employee added")
+        console.table("employee added");
+        setList();
         runSearch();
-    })
+    }) 
 })
 }
 // function to add depatments to the company
 function adddepartment() {
-    inquirer.prompt9([{
+    inquirer.prompt([{
         type: "input",
         message: "what department would you like to add",
         name: "department"
@@ -132,6 +216,7 @@ function adddepartment() {
             connection.query("INSERT INTO department (name) VALUES (?)", [answer.department] , function(err, res) {
             if (err) throw err;
             console.table(res + "department added")
+            setList();
             runSearch();
     })
     })
@@ -149,13 +234,22 @@ function addroles() {
         name: "salary"
     },
     {
-        type: "input",
-        message: "what is the departments id",
-        name: "department_id"
+        type: "list",
+        message: "what is the department id number?",
+        choices: deptArr,
+        name: "department"
+
     }]).then(function(answer){
-        connection.query("INSERT INTO role (title, salary, depatment_id) VALUES (?, ?, ?)", [answer.role, answer.salary, answer.department_id], function(err, res) {
+        
+        for (i=0; i < deptArr.length; i++){
+            if (answer.department == deptArr[i].name){
+                department = deptArr[i].id;
+            }
+        }
+        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.role, answer.salary, department], function(err, res) {
             if (err) throw err;
             console.log(res + "role added")
+            setList();
             runSearch();
         })
     })
@@ -165,7 +259,7 @@ function viewCompany() {
     inquirer.prompt([{
         type: "list",
         message: "how would you like to view the company?",
-        choices: ["by department", "by the roles", "by employees", "view department budgets"],
+        choices: ["by department", "by the roles", "by employees", " view employees by managers"],
         name: "view_type"
     }]).then(function (input) {
         console.log('User selected ' + input.view_type);
@@ -180,14 +274,17 @@ function viewCompany() {
         } else if (input.view_type === 'by employees') {
             byEmployee();
 
-        } else if (input.view_type === 'view department budgets') {
-            viewBudget();
+        // } else if (input.view_type === 'view department budgets') {
+        //     viewBudget();
+        
+        } else if (input.view_type === ' view employees by managers') {
+            byManager();
         }
     })
 }
 // query results by department
 function byDepartment() {
-    connection.query("SELECT * FROM department", function(err, res) {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", function(err, res) {
       if (err) throw err;
       console.table(res);
       runSearch();
@@ -195,7 +292,7 @@ function byDepartment() {
 }
 // query results by roles
 function byRoles() {
-    connection.query("SELECT * FROM role", function(err, res) {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
         if (err) throw err;
         console.table(res);
         runSearch();
@@ -210,12 +307,22 @@ function byEmployee(){
       })
 }
 function byManager() {
-    
+    connection.query("SELECT * FROM employee ORDER BY manager_id", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        runSearch();
+      })
 }
 // query results by departments showing budget
-function viewBudget() {
+// function viewBudget() {
+//        connection.query("SELECT department_id AS id department.name AS department SUM(salary) AS budget FROM  role JOIN department ON role.department_id = department.id GROUP BY  department_id;"), 
+//        function(err, res) {
+//        if (err) throw err;
+//        console.table(res)
+//        runSearch();
+//        }
 
-}
+// }
 // function to select what you would like to update
 function updateCompany() {
     inquirer.prompt([{
@@ -240,25 +347,38 @@ function updateCompany() {
 }
 // function to update the employye by first and last name
 function updateEmployee() {
+    // console.log(employeeArr)
     inquirer.prompt([{
-        type: "input",
-        message: "which employee would you like to update, first name please",
-        name: "employNameFirst"
+        type: "list",
+        message: "which employee would you like to update",
+        choices: employeeArr,
+        name: "employName"
     },
     {
-        type: "input",
-        message: "last name please",
-        name: "employNameLast"
-    },
-    {
-        type: "input",
-        message: "what role would you like them to be in now?",
-        name: "updateRole"
+        type: "list",
+        message: "what is there new role id?",
+        choices: roleArr,
+        name: "role"
 
     }]).then(function(answer) {
-        connection.query("UPDATE SET role_id= ? WHERE first_name= ? AND last_name= ?", [answer.updateRole, answer.employnameFirst, answer.employnameLast], function(err, res) {
+        let employName;
+        for (i=0; i < employeeArr.length; i++){
+            if (answer.employName == employeeArr[i].name){
+                employID = employeeArr[i].id;
+            }
+        }
+     let roleID2;
+        for (i=0; i < roleArr.length; i++){
+            // console.log(answer.role + "  " + roleArr[i].name)
+            if (answer.role == roleArr[i].name){
+                roleID2 = roleArr[i].id;
+            }
+        }
+        connection.query("UPDATE employee SET role_id= ? WHERE id= ?", [roleID2, employID], function(err, res) {
             if (err) throw err;
-            console.table(res + "updated employee")
+            console.log(roleID2)
+            console.log(employID)
+            console.table("updated employee")
             runSearch();
         })
     })
@@ -266,8 +386,41 @@ function updateEmployee() {
 }
 // function to updat ethe managers information
 function updateManagers() {
+    inquirer
+    .prompt([{
+      type: "list",
+      message: "Please select the [EMPLOYEE] you'd like to update: ",
+      choices: employeeArr,
+      name: "employee"
+    },
+    {
+      type: "list",
+      message: "Please select the employee's updated MANAGER",
+      choices: managerArr,
+      name: "manager"
+    }]).then(function(answer) {
+
+        let employID;
+        for (i=0; i < employeeArr.length; i++){
+            if (answer.employee == employeeArr[i].name){
+                employID = employeeArr[i].id;
+            }
+        }
+        let managerID;
+        for (i=0; i < managerArr.length; i++){
+            if (answer.manager == managerArr[i].name){
+                managerID = managerArr[i].id;
+            }
+        }
+        connection.query("UPDATE employee SET manager_id= ? WHERE id= ?", [managerID, employID], function(err, res) {
+            if (err) throw err;
+            console.table("updated employee")
+            runSearch();
+        })
+    })
 
 }
+    
 // function for deleting information form the company
 function deleteFromCompany() {
     inquirer.prompt([{
@@ -298,14 +451,16 @@ function deleteFromCompany() {
 function  deleteEmployee() {
     inquirer.prompt([{
 
-        type: "input",
-        message: "what is there role id?",
-        name: "roleId"
+        type: "list",
+        message: "which employee would you like to update",
+        choices: employeeArr,
+        name: "fired"
+    
 
     }]).then(function(answer) {
-        connection.query("DELETE FROM employee WHERE role_Id= ?", [answer.roleId], function(err, res) {
+        connection.query("DELETE FROM employee WHERE last_name= ?", [answer.fired], function(err, res) {
             if (err) throw err;
-            console.table(res + "deleted employee")
+            console.table("deleted employee")
             runSearch();
         })
     })
@@ -315,30 +470,44 @@ function  deleteEmployee() {
 function deleteRole() {
     inquirer.prompt([{
 
-        type: "input",
-        message: "what is the role id?",
-        name: "roleId"
+            type: "list",
+            message: "what role would you like to delete",
+            choices: roleArr,
+            name: "role1"
+    
 
     }]).then(function(answer) {
-        connection.query("DELETE FROM role WHERE id= ?", [answer.roleId], function(err, res) {
+        
+        for (i=0; i < roleArr.length; i++){
+            if (answer.role1 == roleArr[i].name){
+                roleID = roleArr[i].id;
+            }
+        }
+        connection.query("DELETE FROM role WHERE id= ?", [roleID], function(err, res) {
             if (err) throw err;
-            console.table(res + "deleted role")
+            console.table("deleted role")
             runSearch();
         })
     })
 
 }
-
 // function to delete a department
 function deleteDepartment() {
     inquirer.prompt([{
 
-        type: "input",
+        type: "list",
         message: "what is there department id?",
-        name: "departmentId"
+        choices: deptArr,
+        name: "department"
 
     }]).then(function(answer) {
-        connection.query("DELETE FROM department WHERE id= ?", [answer.departmentId], function(err, res) {
+
+        for (i=0; i < deptArr.length; i++){
+            if (answer.department == deptArr[i].name){
+                deptID = deptArr[i].id;
+            }
+        }
+        connection.query("DELETE FROM department WHERE id= ?", [deptID], function(err, res) {
             if (err) throw err;
             console.table(res + "deleted department")
             runSearch();
@@ -346,7 +515,3 @@ function deleteDepartment() {
     })
 
 }
-
-// function editCompany() {
-
-// }
