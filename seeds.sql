4566c42c6ea97f11c01d884bab198514e72d74a0
Jack W. Tussing <58670450+tussingj89@users.noreply.github.com>
2021-02-14 09:55:42 -0500

    Update seeds.sql
    updated seeds l found some rouge commas as well as a miss created update

--- seeds.sql
+++ seeds.sql
@@ -7,11 +7,12 @@ VALUE ("manager", 75000.00, 2), ("engineer", 52000, 3), ("accountant", 62500, 4)
 INSERT INTO employee (first_name, last_name, role_id)
 VALUE 
  ("John", "Smith", 2), 
- ("Bill", "Gates", 2),
+ ("Bill", "Gates", 2), 
  ("Jack", "Tussing", 3), 
  ("Steve", "Jobs", 5), 
  ("Andrew", "Young", 4), 
  ("Bernie", "Sanders", 1), 
  ("Micheal", "Meyers", 1); 
 
- UPDATE `employee`.`employees` SET `manager_id` = '1' WHERE (`id` > '1');
+ UPDATE `company_db`.`employee` SET `manager_id` = '1' WHERE (`id` > '1');
+ 
