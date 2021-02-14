aed7d2d98f7eb6c814966a1318b7d608a2565440
Jack W. Tussing <58670450+tussingj89@users.noreply.github.com>
2021-02-14 09:54:13 -0500

    Update schema.sql
    updated the schema changed the order of the table creation so the foreign keys work

--- schema.sql
+++ schema.sql
@@ -1,17 +1,8 @@
-DROP IF DATABASE EXISTS company_db;
+DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE employee (
-id INT AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT,
-manager_id INT,
-FOREIGN KEY(role_id) REFERENCES roles(id),
-FOREIGN KEY(manager_id) REFERENCES employees(id)
-);
 
 CREATE TABLE department (
 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -23,5 +14,15 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
-FOREIGN KEY(department_id) REFERENCES departments(id)
+FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+FOREIGN KEY(role_id) REFERENCES role(id),
+FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
