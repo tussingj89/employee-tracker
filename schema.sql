605a6bdc6155ee59db125ffc4ede015f5e635197
Jack W. Tussing <58670450+tussingj89@users.noreply.github.com>
2021-02-14 09:09:33 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -7,21 +7,21 @@ CREATE TABLE employee (
 id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-role_id INT
-manager_id INT
+role_id INT,
+manager_id INT,
 FOREIGN KEY(role_id) REFERENCES roles(id),
 FOREIGN KEY(manager_id) REFERENCES employees(id)
 );
 
 CREATE TABLE department (
 id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30),
+name VARCHAR(30)
 );
 
 CREATE TABLE role (
 id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
-department_id INT
+department_id INT,
 FOREIGN KEY(department_id) REFERENCES departments(id)
 );
